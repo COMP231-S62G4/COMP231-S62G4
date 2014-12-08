c05873cc7af168ae463a6d81c890961fbeafdbce
Sunny Gupta <gupta.sunny664@gmail.com>
2014-12-07 20:08:41 -0500

    Code to verify security question and answers before resetting password.
    Task #90

--- WeMeet/src/comp231/g4/wemeet/AuthenticationActivity.java
+++ WeMeet/src/comp231/g4/wemeet/AuthenticationActivity.java
@@ -6,8 +6,10 @@ import comp231.g4.wemeet.helpers.PasswordRecoveryDataSource;
 import comp231.g4.wemeet.helpers.ValidationHelper;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -15,74 +17,164 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class AuthenticationActivity extends Activity implements OnClickListener {
 	private EditText etPassword;
 	private Button btnEnter;
+	private HashMap<String, String> questionAnswers;
 	public static final String KEY_PASSWORD = "APPLICATION_HASH";
-	
+
+	private Dialog dialogResetPsssword;
+	private TextView tvQuestion1, tvQuestion2, tvQuestion3;
+	private EditText etAnswer1, etAnswer2, etAnswer3;
+	private Button btnReset, btnCancel;
+
+	private SharedPreferences prefs;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_authentication);
-		
-		//initialize components
+
+		// initialize components
 		InitializeComponents();
 	}
 
 	private void InitializeComponents() {
-		
+
 		etPassword = (EditText) findViewById(R.id.etPassword);
 		btnEnter = (Button) findViewById(R.id.btnEnter);
-		
+
 		btnEnter.setOnClickListener(this);
+
+		// loading qa from db if user already have donw password setup
+		prefs = PreferenceManager
+				.getDefaultSharedPreferences(getApplicationContext());
+		if (prefs.contains(SettingsFragment.KEY_SECURITY_SETUP)) {
+			loadSecurityQA();
+
+			// initializing reset password dialog
+			dialogResetPsssword = new Dialog(this);
+			dialogResetPsssword.setContentView(R.layout.dialog_setup_password);
+
+			tvQuestion1 = (TextView) dialogResetPsssword
+					.findViewById(R.id.tvQuestion1);
+			tvQuestion2 = (TextView) dialogResetPsssword
+					.findViewById(R.id.tvQuestion2);
+			tvQuestion3 = (TextView) dialogResetPsssword
+					.findViewById(R.id.tvQuestion3);
+
+			etAnswer1 = (EditText) dialogResetPsssword
+					.findViewById(R.id.etAnswer1);
+			etAnswer2 = (EditText) dialogResetPsssword
+					.findViewById(R.id.etAnswer2);
+			etAnswer3 = (EditText) dialogResetPsssword
+					.findViewById(R.id.etAnswer3);
+
+			btnCancel = (Button) dialogResetPsssword
+					.findViewById(R.id.btnCancel);
+			btnCancel.setOnClickListener(this);
+
+			btnReset = (Button) dialogResetPsssword.findViewById(R.id.btnSetup);
+			btnReset.setText("Reset");
+			btnReset.setOnClickListener(this);
+		}
 	}
 
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.btnEnter:
-			//code to verify password
+			// code to verify password
 			String password = etPassword.getText().toString();
 			String encodedPassword = ValidationHelper.EncodeString(password);
-			
+
 			etPassword.setText("");
-			
-			SharedPreferences pManager = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+			SharedPreferences pManager = PreferenceManager
+					.getDefaultSharedPreferences(getApplicationContext());
 			String userPassword = pManager.getString(KEY_PASSWORD, "");
-			if(encodedPassword.equals(userPassword)){
+			if (encodedPassword.equals(userPassword)) {
 				// code to launch FriendsNearBy activity
 				Intent i = new Intent(AuthenticationActivity.this,
 						HomeActivity.class);
 				i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
 				i.addFlags(Intent.FLAG_ACTIVITY_TASK_ON_HOME);
 				startActivity(i);
-				
-				Toast.makeText(AuthenticationActivity.this, "Entering into secure zone!", Toast.LENGTH_SHORT).show();
-			}else{
+
+				Toast.makeText(AuthenticationActivity.this,
+						"Entering into secure zone!", Toast.LENGTH_SHORT)
+						.show();
+			} else {
 				etPassword.setError("Invalid password!");
 				etPassword.setBackgroundColor(Color.parseColor("#C20000"));
 				etPassword.setTextColor(Color.WHITE);
-				Toast.makeText(AuthenticationActivity.this, "Invalid password!", Toast.LENGTH_SHORT).show();
+				Toast.makeText(AuthenticationActivity.this,
+						"Invalid password!", Toast.LENGTH_SHORT).show();
 			}
 			break;
+		case R.id.btnCancel:
+			if (dialogResetPsssword != null) {
+				dialogResetPsssword.dismiss();
+			}
+			break;
+		case R.id.btnSetup:
+			if (dialogResetPsssword != null) {
+				if (validateAnswers()) {
 
+					String answer1 = etAnswer1.getText().toString().trim();
+					String answer2 = etAnswer2.getText().toString().trim();
+					String answer3 = etAnswer3.getText().toString().trim();
+
+					String question1 = tvQuestion1.getText().toString().trim();
+					String question2 = tvQuestion2.getText().toString().trim();
+					String question3 = tvQuestion3.getText().toString().trim();
+
+					if (questionAnswers.get(question1).equals(answer1)
+							&& questionAnswers.get(question2).equals(answer2)
+							&& questionAnswers.get(question3).equals(answer3)) {
+
+						dialogResetPsssword.dismiss();
+					}
+
+				}
+			}
+			break;
 		default:
 			break;
 		}
 	}
-	
-	private HashMap<String, String> getSecurityQA(){
-		HashMap<String, String> qa = null;
-		
-		PasswordRecoveryDataSource dsPasswordRecovery = new PasswordRecoveryDataSource(this);
+
+	private void loadSecurityQA() {
+		PasswordRecoveryDataSource dsPasswordRecovery = new PasswordRecoveryDataSource(
+				this);
 		dsPasswordRecovery.open();
-		
-		qa = dsPasswordRecovery.getAll();
-		
+
+		questionAnswers = dsPasswordRecovery.getAll();
+
 		dsPasswordRecovery.close();
-		
-		return qa;
+	}
+
+	private boolean validateAnswers() {
+		String answer1 = etAnswer1.getText().toString().trim();
+		String answer2 = etAnswer2.getText().toString().trim();
+		String answer3 = etAnswer3.getText().toString().trim();
+
+		if (answer1.length() == 0) {
+			etAnswer1.setError("Invalid answer!");
+			return false;
+		}
+		if (answer2.length() == 0) {
+			etAnswer2.setError("Invalid answer!");
+			return false;
+		}
+		if (answer3.length() == 0) {
+			etAnswer3.setError("Invalid answer!");
+			return false;
+		}
+
+		return true;
 	}
 }
